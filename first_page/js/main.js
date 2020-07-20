b5709c29b21d6b3761753d40de58f9ba1d9b4c5b
DimaSilver <68532590+DimaSilver@users.noreply.github.com>
2020-07-20 11:27:12 +0300

    I added page making of three pages of one site.
    HTML + CSS + JS

---
+++ first_page/js/main.js
@@ -0,0 +1,5 @@
+$(function(){
+	$('.header-btn').on('click', function(){
+		$('.menu__list').slideToggle();
+	});
+});
---
+++ second_page/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+	$('.recent-post__block').on('click', function(){
+		$('.recent-post__block').removeClass('active__post');
+		$(this).toggleClass('active__post');
+		
+	});
+	$('.category__li').on('click', function(){
+		$('.category__li').removeClass('active__category');
+		$(this).toggleClass('active__category');
+		
+	});
+	$('.tags').on('click', function(){
+		$('.tags').removeClass('active__tag');
+		$(this).toggleClass('active__tag');
+		
+	});
+	$('.bottom-button').on('click', function(){
+		$('.bottom-button').removeClass('active__button');
+		$(this).toggleClass('active__button');
+		
+	});
+	$('.header-btn').on('click', function(){
+		$('.menu__list').slideToggle();
+	});
+});
